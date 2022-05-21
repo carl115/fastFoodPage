b3de534f2e6d2370208f8e48ec338a4615c4ac04
Carl <cfds2003@gmail.com>
2022-05-21 14:35:04 -0500

    Responsive 768, 425, 375, 320

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const btnWindow = document.getElementById('btn-to-accept');
 const btnSearch = document.getElementById('btn-search');
 const barSearch = document.getElementById('bar-search');
 const btnCancelSrc = document.getElementById('cancel');
+const menu = document.querySelector('nav');
+const btnMenu = document.querySelector('.btn-menu');
 
 function windowHidden() {
     windowInit.style.display = 'none';
@@ -19,3 +21,7 @@ function cancelSearch() {
 btnWindow.addEventListener('click', windowHidden);
 btnSearch.addEventListener('click', searchBar);
 btnCancelSrc.addEventListener('click', cancelSearch);
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
